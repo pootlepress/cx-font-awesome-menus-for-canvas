335a4229d4c7053f1230032f2b58fbf5d9476a09
Alan Ng <jenfoong@gmail.com>
2014-07-18 16:30:22 +0800

    Add Updater

--- pootlepress-fontawesome-menu.php
+++ pootlepress-fontawesome-menu.php
@@ -17,8 +17,21 @@ require_once( 'classes/class-pootlepress-fontawesome-menu.php' );
 require_once('classes/class-main-nav-walker.php');
 require_once('classes/class-top-nav-walker.php');
 require_once('classes/class-widget-nav-walker.php');
+require_once( 'classes/class-pootlepress-updater.php');
 
 $GLOBALS['pootlepress_fontawesome_menu'] = new Pootlepress_FontAwesome_Menu( __FILE__ );
 $GLOBALS['pootlepress_fontawesome_menu']->version = '1.2';
 
+add_action('init', 'pp_fam_updater');
+function pp_fam_updater()
+{
+    if (!function_exists('get_plugin_data')) {
+        include(ABSPATH . 'wp-admin/includes/plugin.php');
+    }
+    $data = get_plugin_data(__FILE__);
+    $wptuts_plugin_current_version = $data['Version'];
+    $wptuts_plugin_remote_path = 'http://www.pootlepress.com/?updater=1';
+    $wptuts_plugin_slug = plugin_basename(__FILE__);
+    new Pootlepress_Updater ($wptuts_plugin_current_version, $wptuts_plugin_remote_path, $wptuts_plugin_slug);
+}
 ?>
